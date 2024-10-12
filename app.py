68693e2aecb186fde7c8fbf9718b2c046b6d47bb
adishar93 <adishar93@gmail.com>
2024-10-12 15:35:39 -0700

    Implemented basic signup and login

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, request, jsonify
+from flask_pymongo import PyMongo
+from flask_bcrypt import Bcrypt
+from config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+mongo = PyMongo(app)
+bcrypt = Bcrypt(app)
+
+# Signup API
+@app.route('/api/signup', methods=['POST'])
+def signup():
+    data = request.get_json()
+    if mongo.db.users.find_one({"username": data['username']}):
+        return jsonify(success=False, message="User already exists"), 400
+    
+    hashed_password = bcrypt.generate_password_hash(data['password']).decode('utf-8')
+    user_data = {
+        "username": data['username'],
+        "name": data['name'],
+        "age": data['age'],
+        "language": data['language'],
+        "password": hashed_password,
+        "working_professional": data['working_professional']
+    }
+    mongo.db.users.insert_one(user_data)
+    return jsonify(success=True, message="Signup successful"), 201
+
+# Login API
+@app.route('/api/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    user = mongo.db.users.find_one({"username": data['username']})
+    
+    if user and bcrypt.check_password_hash(user['password'], data['password']):
+        return jsonify(success=True, message="Login successful"), 200
+    
+    return jsonify(success=False, message="Invalid credentials"), 401
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ config.py
@@ -0,0 +1,4 @@
+import os
+
+class Config:
+    MONGO_URI = "mongodb+srv://adishar93:wGZ7DtIjbuKT7u8K@cluster0.jwcsneo.mongodb.net/morpheus?retryWrites=true&w=majority&appName=Cluster0"
